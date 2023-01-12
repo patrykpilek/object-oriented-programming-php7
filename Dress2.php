c29b08c5d224a31292cde0049d067697a2c20776
Patryk Pilek <patryk.pilek@gmail.com>
2023-01-12 22:34:11 +0000

    visibility

---
+++ Dress2.php
@@ -0,0 +1,32 @@
+<?php
+
+class Dress
+{
+    public $color = "red";  // The color of the dress
+    private $fabric = "linen"; // The fabric of the dress
+    private $design = "Slim Fit Blazer";//The design of the dress
+
+    public function displayInfo()
+    {
+        echo "The info about the dress.";
+        echo $this->color;
+        echo $this->fabric;
+        echo $this->design;
+    }
+
+    private function helloWorld($number1, $number2)
+    {
+        return $number1 + $number2;
+    }
+}
+
+$dressObj = new Dress();
+
+echo $dressObj->fabric . "</br>";
+echo $dressObj->design . "</br>";
+
+$dressObj->helloWorld();
+
+
+
+
