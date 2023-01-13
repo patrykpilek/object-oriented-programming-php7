7b5f61dbf4ff551a4c32600187a7d1c6865b1cbc
Patryk Pilek <patryk.pilek@gmail.com>
2023-01-13 23:41:42 +0000

    final keyword

---
+++ Final-keyword.php
@@ -0,0 +1,17 @@
+<?php
+
+final class A
+{
+    final public function test()
+    {
+        echo "Class A test method.</br>";
+    }
+}
+
+class B extends A
+{
+    public function test()
+    {
+        echo "Class B test method.</br>";
+    }
+}
